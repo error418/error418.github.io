a692daf84dc773ac4a164bafcda0803b907902a9
error418 <gerbigmic+github@googlemail.com>
2016-06-28 20:11:27 +0200

    * switch to gh-repos
    switched to gh-repos implementation.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,7 @@
-angular.module('githubRepositories', []);
-angular.module("main", ['ngResource', 'githubRepositories'])
+angular.module("main", ['ghRepositories'])
 
-.factory('Repositories', ['$resource', function($resource) {
-	return $resource(
-				'/users/:user/repos', 
-				null,
-			    {
-			        get: {
-			        	url: 'https://api.github.com/users/:user/repos',
-			        	method:'GET',
-			        	isArray: true
-			        }
-			    }
-	);
-}])
 
-.controller("MainController", ['Repositories', '$filter', '$scope', function (Repositories, $filter, $scope) {
-	Repositories.get(
-			{user: "error418"},
-			function (success) {
-				$scope.repos = success;
-			}
-	);
-}]);
+
+.controller("MainController", function () {
+
+});
